b351652b537e1c89af91363ea29b9ba328851dda
Karthik J <113295402+Karthikj498@users.noreply.github.com>
2023-09-22 20:37:10 +0530

    Update models.py

--- myworld/members/models.py
+++ myworld/members/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 BRANCH_CHOICES = (
     ("BA", "BA"),
@@ -7,7 +8,6 @@ BRANCH_CHOICES = (
     ("CA", "CA"),
 )
 
-
 # Create your models here.
 class Students(models.Model):
     first_name = models.CharField(max_length=200)
@@ -15,18 +15,22 @@ class Students(models.Model):
     address = models.CharField(max_length=200)
     roll_number = models.IntegerField()
     mobile = models.CharField(max_length=10)
-    branch = models.CharField(max_length=10, choices=BRANCH_CHOICES, null=True)
+    branch = models.CharField(max_length=10, choices=BRANCH_CHOICES)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=500)
-    release_date = models.DateTimeField('Realse Date')
-    blog_time = models.CharField(max_length=50)
-    author = models.CharField(max_length=200)
-    created_date = models.DateTimeField('Created Date', auto_now_add=True, null=True)
+    title = models.CharField(max_length=500, default="")  # Default value for 'title'
+    release_date = models.DateTimeField('Release Date', default=timezone.now)  # Default value for 'release_date'
+    blog_time = models.CharField(max_length=50, default="")  # Default value for 'blog_time'
+    author = models.CharField(max_length=200, default="")  # Default value for 'author'
+    content = models.TextField(default="")  # Default value for 'content'
+    total_comments = models.IntegerField(default=0)  # Default value for 'total_comments'
+    recommended_on_google = models.BooleanField(default=False)  # Default value for 'recommended_on_google'
+    file_path_html = models.CharField(max_length=500, default="")  # Default value for 'file_path_html'
+    created_date = models.DateTimeField('Created Date', default=timezone.now)  # Default value for 'created_date'
 
     def __str__(self):
         return self.title
