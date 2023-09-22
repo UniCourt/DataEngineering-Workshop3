b755c35b6beb2266814c36a79fda185fc7b16f6d
Karthik J <113295402+Karthikj498@users.noreply.github.com>
2023-09-22 20:29:53 +0530

    Update web_scrapper.py

--- myworld/web_scrapper.py
+++ myworld/web_scrapper.py
@@ -1,26 +1,41 @@
 import psycopg2
 import requests
-import re
-from bs4 import BeautifulSoup, element
+from bs4 import BeautifulSoup
+from datetime import datetime
 
-# For the credentials mentioned below, you may refer the docker-compose.yml present in myworld .
+# For the credentials mentioned below, you may refer to the docker-compose.yml present in myworld.
 db_name = 'member_db'
 db_user = 'postgres'
 db_pass = '123456'
-db_host = 'psql-db'
+db_host = 'psql-db_1'
 db_port = '5432'
 
-# This will create the connection the to postgres database.
+# This will create the connection to the PostgreSQL database.
 conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_pass, host=db_host, port=db_port)
 
 
-def add_row_to_blog(title, author, date, time):
-    # This function will add the entry to database
-    sql = """INSERT INTO members_blog (title, release_date, blog_time, author, created_date) VALUES (%s, %s::DATE, %s::TIME, %s, NOW())"""
+def add_row_to_blog(title, author, date, time, content, total_comments, recommended_on_google, file_path_html):
+    # Parse the time value and convert it to the 'HH:MI:SS' format
+    parsed_time = datetime.strptime(time, '%I:%M %p').strftime('%H:%M:%S')
 
+    # Convert total_comments to an integer or use a default value if "No comments found"
+    if total_comments.isdigit():
+        total_comments = int(total_comments)
+    else:
+        total_comments = 0
+
+    # Convert recommended_on_google to a boolean (True or False) based on your logic
+    # For example, if it's "recommended," set it to True; otherwise, set it to False
+    recommended_on_google = True if recommended_on_google == "recommended" else False
+
+    # This function will add the entry to the database
+    sql = """
+    INSERT INTO members_blog (title, release_date, blog_time, author, created_date, content, total_comments, recommended_on_google, file_path_html)
+    VALUES (%s, %s::DATE, %s::TIME, %s, NOW(), %s, %s, %s, %s)
+    """
     with conn:
         with conn.cursor() as curs:
-            curs.execute(sql, (title, date, time, author))
+            curs.execute(sql, (title, date, parsed_time, author, content, total_comments, recommended_on_google, file_path_html))
 
 
 def truncate_table():
@@ -61,17 +76,34 @@ def start_extraction():
         author = post_footer.select('.post-author span')[0].text
 
         time = post_footer.select('abbr')[0].text
-        # Inserting data into database
-        add_row_to_blog(title, author, date, time)
+
+        # New code to scrape additional information
+        content = post.select('.post-body')[0].text.strip()  # Content
+
+        # Check if there are elements with the comment-link span selector
+        comment_link_span = post_footer.select('.comment-link span')
+        if comment_link_span:
+            total_comments = comment_link_span[0].text  # Total Comments
+        else:
+            total_comments = "No comments found"
+
+        # Add logic to scrape "recommended this on google" and "file path of saved HTML" here
+        recommended_on_google = ""  # Replace with actual logic
+        file_path_html = ""  # Replace with actual logic
+
+        # Inserting data into the database
+        add_row_to_blog(title, author, date, time, content, total_comments, recommended_on_google, file_path_html)
 
         print("\nTitle:", title.strip('\n'))
-        print("Date:", date, )
+        print("Date:", date)
         print("Time:", time)
         print("Author:", author)
+        print("Content:", content)
+        print("Total Comments:", total_comments)
+        print("Recommended on Google:", recommended_on_google)  # Print the value
+        print("File saved at:", file_path_html)  # Print the value
 
-        # print("Number of blogs read:", count)
-        print(
-            "\n---------------------------------------------------------------------------------------------------------------\n")
+        print("\n---------------------------------------------------------------------------------------------------------------\n")
 
 
 if __name__ == "__main__":
